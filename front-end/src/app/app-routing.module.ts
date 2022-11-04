8da07db1be65ace178320e302ed40cda011feef2
Shaik-Mehraj <shaikmehraj2001@gmail.com>
2022-11-04 11:12:22 +0530

    header

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/forms/login/login.component';
+import { HeaderComponent } from './components/partials/header/header.component';
 
-const routes: Routes = [];
+ const routes: Routes = [
+  {path:'login',component:LoginComponent},
+ ]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const myComponents = [
+  HeaderComponent,
+  LoginComponent
+]
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule, myComponents } from './app-routing.module';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    myComponents
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
